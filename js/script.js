5d44d9f04b37b928280fbc81c814cbf57124b4e6
Daniel Strong <danielpstrong@gmail.com>
2016-07-08 21:36:44 -0400

    wrote some javascript to prevent scrolling when mobile overlay is open

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
-function closeMenu() {
-    document.getElementById('nav-toggle').checked = false;
-}
-
+var body = document.body;
+var burger = document.getElementById('nav-toggle');
+    burger.addEventListener('click', disableScroll);
 var link = document.querySelectorAll('.mobile-nav-list li a');
 
 for (i = 0; i < link.length; i++) {
     link[i].addEventListener('click', closeMenu);
+}
+
+// Closes mobile nav overlay when a link is clicked
+function closeMenu() {
+    body.className = '';
+    burger.checked = false;
+}
+
+// Disables scrolling when mobile nav overlay is open
+function disableScroll() {
+    if (burger.checked) {
+        body.className = 'disable-scroll';
+    }
+    else {
+        body.className = '';
+    }
 }
